db54ba5c1f0343be169c158a4a5a7e3cbf2d7469
Drawethree <janci.kluka@gmail.com>
2021-10-23 22:52:26 +0200

    Changed actor name

--- RotatingWheel/Program.cs
+++ RotatingWheel/Program.cs
@@ -10,7 +10,7 @@ namespace RotatingWheel
         {
             while (true)
             {
-                Console.Write(TocKolieskom("janko"));
+                Console.Write(TocKolieskom("jurko"));
                 Console.SetCursorPosition(0, 0);
                 Thread.Sleep(100);
             }
